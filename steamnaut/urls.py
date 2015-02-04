fb2795557ed32d61d2a1d07b7b6a1100259a113b
Ben Junya <bjunya@gmail.com>
2015-02-04 08:27:54 -0800

    Added home page index to url and views. Added index template for homepage. Added parked page under construction

--- steamnaut/urls.py
+++ steamnaut/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'steamnaut.views.home', name='home'),
+    url(r'^$', 'steamnaut.views.index', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^videos/', include('videos.urls')),
     url(r'^admin/', include(admin.site.urls)),
